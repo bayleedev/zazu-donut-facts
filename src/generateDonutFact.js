80395c2b546d494ddf24fdec8d1501a7c2d54c49
Blaine Schmeisser <blainesch@gmail.com>
2017-04-18 17:45:50 -0700

    Add more conspiracies.

--- src/generateDonutFact.js
+++ src/generateDonutFact.js
@@ -4,6 +4,10 @@ const donuts = [
   'The world isn\'t flat or round, it\'s donut.',
   'DonutScript compiles to Java, not JavaScript.',
   'Portland has an underground donut bar.',
+  'The world is ran by a group of donuts.',
+  'NASA never landed on the donut.',
+  'Donuts wiretapped Trump first.',
+  'UFOs are just our Donut overlords.',
 ]
 const randDonut = () => {
   const index = Math.round(Math.random() * 1000) % donuts.length
